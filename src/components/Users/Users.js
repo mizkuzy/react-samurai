7b29f2c5ce00dbb3bf1016d0353accd14f4f6529
lyudmila.rudkova@kandasoft.com <MisteR2901>
2020-09-02 22:19:29 +0300

    Wrap Users component in a container component to make Users pure function

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,8 +1,6 @@
 import React from "react";
-import UserItem from "./UserItem/UserItem";
 import s from "./Users.module.css";
-import * as axios from "axios";
-import { USERS_URL } from "../../util/api-urls";
+import UserItem from "./UserItem/UserItem";
 
 const Paging = ({
   totalElementsNumber,
@@ -14,14 +12,16 @@ const Paging = ({
 
   return [...Array(pagesCount).keys()].map((p) => {
     const normalisedPageNumber = p + 1;
-
     return (
       <span
         key={p}
         onClick={() => {
+            debugger
           onChangePage(normalisedPageNumber);
         }}
-        className={pageNumber === normalisedPageNumber && s.selected_page}
+        className={
+          (pageNumber === normalisedPageNumber && s.selected_page) || ""
+        }
       >
         {normalisedPageNumber}{" "}
       </span>
@@ -29,48 +29,36 @@ const Paging = ({
   });
 };
 
-class Users extends React.Component {
-  componentDidMount() {
-    this.fetchUsers(this.props.pageNumber);
-  }
-
-  fetchUsers(pageNumber) {
-    const usersUrl = `${USERS_URL}?count=${this.props.pageSize}&page=${pageNumber}`;
-
-    axios.get(usersUrl).then((response) => {
-      this.props.setTotalUsersCount(response.data.totalCount);
-      this.props.setUsers(response.data.items);
-    });
-  }
-
-  onChangePage = (pageNumber) => {
-    this.props.updatePageNumber(pageNumber);
-    this.fetchUsers(pageNumber);
-  };
-
-  render() {
-    return (
-      <div className={s.users_content}>
-        <h3>Users</h3>
-        <Paging
-          totalElementsNumber={this.props.totalUsersCount}
-          pageSize={this.props.pageSize}
-          pageNumber={this.props.pageNumber}
-          onChangePage={this.onChangePage}
-        />
-        <div className={s.content}>
-          {this.props.users.map((u) => (
-            <UserItem
-              key={u.id}
-              user={u}
-              followUser={this.props.followUser}
-              unfollowUser={this.props.unfollowUser}
-            />
-          ))}
-        </div>
+const Users = ({
+  totalUsersCount,
+  pageSize,
+  pageNumber,
+  onChangePage,
+  users,
+  followUser,
+  unfollowUser,
+}) => {
+  return (
+    <div className={s.users_content}>
+      <h3>Users</h3>
+      <Paging
+        totalElementsNumber={totalUsersCount}
+        pageSize={pageSize}
+        pageNumber={pageNumber}
+        onChangePage={onChangePage}
+      />
+      <div className={s.content}>
+        {users.map((u) => (
+          <UserItem
+            key={u.id}
+            user={u}
+            followUser={followUser}
+            unfollowUser={unfollowUser}
+          />
+        ))}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Users;
--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,5 +1,4 @@
 import { connect } from "react-redux";
-import Users from "./Users";
 import {
   followUserAC,
   setTotalUsersCountAC,
@@ -7,6 +6,44 @@ import {
   unfollowUserAC,
   updatePageNumberAC,
 } from "../../redux/actionCreators";
+import React from "react";
+import { USERS_URL } from "../../util/api-urls";
+import * as axios from "axios";
+import Users from "./Users";
+
+class UsersContainer extends React.Component {
+  componentDidMount() {
+    this.fetchUsers(this.props.pageNumber);
+  }
+
+  fetchUsers(pageNumber) {
+    const usersUrl = `${USERS_URL}?count=${this.props.pageSize}&page=${pageNumber}`;
+
+    axios.get(usersUrl).then((response) => {
+      this.props.setTotalUsersCount(response.data.totalCount);
+      this.props.setUsers(response.data.items);
+    });
+  }
+
+  onChangePage = (pageNumber) => {
+    this.props.updatePageNumber(pageNumber);
+    this.fetchUsers(pageNumber);
+  };
+
+  render() {
+    return (
+      <Users
+        totalUsersCount={this.props.totalUsersCount}
+        pageNumber={this.props.pageNumber}
+        pageSize={this.props.pageSize}
+        onChangePage={this.onChangePage}
+        users={this.props.users}
+        followUser={this.props.followUser}
+        unfollowUser={this.props.unfollowUser}
+      />
+    );
+  }
+}
 
 const mapStateToProps = (state) => {
   const usersPage = state.users;
@@ -37,4 +74,5 @@ const mapDispatchToProps = (dispatch) => {
     },
   };
 };
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
