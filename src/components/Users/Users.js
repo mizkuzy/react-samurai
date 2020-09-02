2d5a3cf3ede586bd22a9aec83478b7f7b044feab
lyudmila.rudkova@kandasoft.com <MisteR2901>
2020-09-02 11:39:26 +0300

    Refactor pure function Users to Class Component

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -4,60 +4,30 @@ import s from "./Users.module.css";
 import * as axios from "axios";
 import { USERS_URL } from "../../util/api-urls";
 
-const Users = ({ users, followUser, unfollowUser, setUsers }) => {
-  if (users.length === 0) {
-    console.log("url", USERS_URL);
-    axios
-      .get(USERS_URL)
-      .then((response) => {
-        console.log("USERS RESPONSE", response);
-        setUsers(response.data.items);
-      })
-      .catch((e) => {
-        console.log("USERS ERROR", e);
-        const users = [
-          {
-            id: 1,
-            followed: false,
-            avaUrl: "",
-            name: "Lucy Ru",
-            status: "Happy",
-            location: { city: "Saint Petersburg", country: "Russia" },
-          },
-          {
-            id: 2,
-            followed: true,
-            name: "Andrew Ru",
-            status: "Happy",
-            location: { city: "Saint Petersburg", country: "Russia" },
-          },
-          {
-            id: 3,
-            followed: true,
-            name: "Anna Mel",
-            status: "ЖывЕ Беларусь!",
-            location: { city: "Minsk", country: "Belarus" },
-          },
-        ];
-        setUsers(users);
-      });
+class Users extends React.Component {
+  componentDidMount() {
+    axios.get(USERS_URL).then((response) => {
+      this.props.setUsers(response.data.items);
+    });
   }
 
-  return (
-    <div className={s.users_content}>
-      <h3>Users</h3>
-      <div className={s.content}>
-        {users.map((u) => (
-          <UserItem
-            key={u.id}
-            user={u}
-            followUser={followUser}
-            unfollowUser={unfollowUser}
-          />
-        ))}
+  render() {
+    return (
+      <div className={s.users_content}>
+        <h3>Users</h3>
+        <div className={s.content}>
+          {this.props.users.map((u) => (
+            <UserItem
+              key={u.id}
+              user={u}
+              followUser={this.props.followUser}
+              unfollowUser={this.props.unfollowUser}
+            />
+          ))}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Users;
