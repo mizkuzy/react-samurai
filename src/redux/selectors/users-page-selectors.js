af8c5b04eb2d8ea63bec88207fb14cd9f8e9bc0b
lyudmila.rudkova@kandasoft.com <MisteR2901>
2020-09-09 13:47:16 +0300

    Add one example using reselector lib

---
+++ src/redux/selectors/users-page-selectors.js
@@ -0,0 +1,9 @@
+import { createSelector } from "reselect";
+
+const getUsers = (state) => {
+  return state.usersPage.users;
+};
+
+export const getUsersSelector = createSelector(getUsers, (users) => {
+  return users;
+});
