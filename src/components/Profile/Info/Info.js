3e40ac578f4bbf9cbce6b8e4bb1eb9433a2aed5f
lyudmila.rudkova@kandasoft.com <MisteR2901>
2020-09-11 15:24:11 +0300

    Refactor Info Component to hooks

--- src/components/Profile/Info/Info.js
+++ src/components/Profile/Info/Info.js
@@ -3,20 +3,27 @@ import s from "./Info.module.css";
 import { get as _get } from "lodash";
 import { DEFAULT_PROFILE_AVATAR_SRC } from "../../../util/constants";
 import Status from "../Status/Status";
+import { useSelector } from "react-redux";
+import { updateStatus } from "../../../redux/thunks";
 
-const Info = ({ profile, status, updateStatus }) => (
-  <>
-    <div>
-      <img
-        alt={"Profile Avatar"}
-        className={s.imgProfile}
-        src={_get(profile, "photos.small", DEFAULT_PROFILE_AVATAR_SRC)}
-      />
-      <Status text={status} updateStatus={updateStatus} />
-      <p>{profile.fullName}</p>
-      <p>About me: {profile.aboutMe}</p>
-    </div>
-  </>
-);
+const Info = () => {
+  const profile = useSelector((state) => state.profilePage.profile);
+  const status = useSelector((state) => state.profilePage.status);
+
+  return (
+    <>
+      <div>
+        <img
+          alt={"Profile Avatar"}
+          className={s.imgProfile}
+          src={_get(profile, "photos.small", DEFAULT_PROFILE_AVATAR_SRC)}
+        />
+        <Status text={status} updateStatus={updateStatus} />
+        <p>{profile.fullName}</p>
+        <p>About me: {profile.aboutMe}</p>
+      </div>
+    </>
+  );
+};
 
 export default Info;
