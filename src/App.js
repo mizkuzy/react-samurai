b9f8ae97ef3c92b26e4113f4b5d8254eccae12e4
lyudmila.rudkova@kandasoft.com <MisteR2901>
2020-09-12 18:35:53 +0300

    Add login form

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import Settings from "./components/Settings/Settings";
 import DialoguesContainer from "./components/Dialogues/DialoguesContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import Login from "./components/Login/Login";
-import Profile from "./components/Profile/Profile";
 import { Provider } from "react-redux";
 import store from "./redux/redux-store";
 import Header from "./components/Header/Header";
+import ProfileHook from "./components/Profile/ProfileHook";
 
 const App = () => (
   <BrowserRouter>
@@ -22,9 +22,9 @@ const App = () => (
         <main className="app-wrapper-content">
           <Route
             exact
-            path={["/", "/profile/:userId?"]}
+            path={["/", "/profile/:userId"]}
             render={() => {
-              return <Profile />;
+              return <ProfileHook />;
             }}
           />
           <Route
@@ -32,11 +32,13 @@ const App = () => (
             path="/dialogues"
             render={() => <DialoguesContainer />}
           />
+          <Route path="/users" render={() => <UsersContainer />} />
+
+          <Route path="/login" render={() => <Login />} />
+
           <Route path="/news" render={() => <News />} />
           <Route path="/music" render={() => <Music />} />
           <Route path="/settings" render={() => <Settings />} />
-          <Route path="/users" render={() => <UsersContainer />} />
-          <Route path="/login" render={() => <Login />} />
         </main>
       </div>
     </Provider>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,49 @@
 import React from "react";
+import { Field, Form, Formik } from "formik";
+
+const LoginForm = () => {
+  return (
+    <Formik
+      initialValues={{
+        login: "",
+        password: "",
+        rememberMe: "",
+      }}
+      onSubmit={async (values) => {
+        await new Promise((r) => setTimeout(r, 500)); // todo do we need it?
+        alert(JSON.stringify(values, null, 2));
+      }}
+    >
+      <Form>
+        <div>
+          <label htmlFor="login">Login</label>
+          <Field id="login" name="login" placeholder="Login" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <Field id="password" name="password" placeholder="Password" />
+        </div>
+        <div>
+          <label>
+            <Field id="rememberMe" type="checkbox" name="rememberMe" />
+            Remember me
+          </label>
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </Form>
+    </Formik>
+  );
+};
 
 const Login = () => {
-  return <h1>LOGIN PAGE</h1>;
+  return (
+    <div>
+      <h1>Login form</h1>
+      <LoginForm />
+    </div>
+  );
 };
 
 export default Login;
